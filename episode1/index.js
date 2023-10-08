3c97ebf69ae1398882a0dd489cfb0767f73ca810
rks-1995 <Ratnakumar.singh@rap.ventures>
2023-10-08 08:36:29 +0530

    episode-01

---
+++ episode1/index.js
@@ -0,0 +1,39 @@
+/*
+ *
+ *
+ *
+ *
+ */
+const heading = React.createElement(
+  "h1",
+  {},
+  "this is a place where you can find the react application"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root")); // virtual dom
+
+/*
+ *
+ * <div class="parent">
+ *      <div class="child">
+ *          <h1></h1>
+ *      </div>
+ * </div>
+ *
+ *
+ */
+
+const nestedHeading = React.createElement("div", { className: "parent" }, [
+  React.createElement(
+    "div",
+    { className: "child1", key: 1 },
+    React.createElement("h1", {}, "Nested")
+  ),
+  React.createElement(
+    "div",
+    { className: "child2", key: 2 },
+    React.createElement("h1", {}, "Nested")
+  ),
+]);
+
+root.render(nestedHeading);
