90db7da36d2b74be3fc7e730f7abb218d71093e8
rks-1995 <Ratnakumar.singh@rap.ventures>
2023-10-10 19:14:05 +0530

    episode-03

--- episode1/App.js
+++ episode1/App.js
@@ -1,41 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-/*
- *
- *
- *
- *
- */
-const heading = React.createElement(
-  "h1",
-  {},
-  "this is a place where you can find the react application"
+
+// this is how you create react element using react
+const heading = React.createElement("h1", {}, "Namaste React");
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// this is how you create react element using JSX
+// jsx is transpiled so that js engine can understand - parcel - babel -> jsx -> react element
+// jsx -> babel transpiled to react.createElement -> JS-object -> html element(root.render());
+const jsxHeading = <h1>Namaste React using JSX</h1>;
+
+// functional component
+const Heading = () => <h1>heading component</h1>;
+
+// component composition - passing component as props to other component
+const HeadingComponent = ({ children }) => (
+  <div id="container">
+    {children} {/* component composition */}
+    <h1>functional component</h1>
+  </div>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root")); // virtual dom
-
-/*
- *
- * <div class="parent">
- *      <div class="child">
- *          <h1></h1>
- *      </div>
- * </div>
- *
- *
- */
-
-const nestedHeading = React.createElement("div", { className: "parent" }, [
-  React.createElement(
-    "div",
-    { className: "child1", key: 1 },
-    React.createElement("h1", {}, "Nested")
-  ),
-  React.createElement(
-    "div",
-    { className: "child2", key: 2 },
-    React.createElement("h1", {}, "Nested")
-  ),
-]);
-
-root.render(nestedHeading);
+root.render(<HeadingComponent children={<Heading />} />);
